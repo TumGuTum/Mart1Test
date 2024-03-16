4c8b7dc2264235a0b8cb215f17a3becaab32e27f
TumGuTum <150485499+TumGuTum@users.noreply.github.com>
2024-03-16 18:05:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -62,3 +62,12 @@ $('.form').on('submit', function (event) {
 
     return false;
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
